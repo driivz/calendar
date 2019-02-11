7437cbfd3e0c4e9686edf095ca7e159cf315c8fe
Bofik <bofik@i.ua>
2019-02-11 20:55:40 +0200

    min height set

--- app/src/main/java/io/github/hidroh/calendar/widget/MonthView.java
+++ app/src/main/java/io/github/hidroh/calendar/widget/MonthView.java
@@ -73,8 +73,6 @@ class MonthView extends RecyclerView {
         setLayoutManager(new GridLayoutManager(getContext(), SPANS_COUNT));
         setHasFixedSize(true);
         setCalendar(CalendarUtils.today());
-        setClipChildren(false);
-        setClipToPadding(false);
     }
 
     /**
